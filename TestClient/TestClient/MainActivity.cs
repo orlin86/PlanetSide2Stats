9177f6de3969b0d25f05c1ff08a4c80c74853a4a
orlin86 <orlin.perfanov@gmail.com>
2017-01-15 14:07:16 +0200

    Upload client, to do explanation

---
+++ TestClient/TestClient/MainActivity.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Widget;
+using Android.OS;
+using WebSocketSharp;
+
+
+namespace TestClient
+{
+    [Activity(Label = "TestClient", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            EditText inputName = FindViewById<EditText>(Resource.Id.InputName);
+            Button submitButton = FindViewById<Button>(Resource.Id.SubmitNameButton);
+            Button disconnecButton = FindViewById<Button>(Resource.Id.DisconnectButton);
+            TextView dataBig = FindViewById<TextView>(Resource.Id.DataBig);
+            TextView dataSmall = FindViewById<TextView>(Resource.Id.DataSmall);
+
+            WebSocket ws = new WebSocket("wss://push.planetside2.com/streaming?environment=ps2&service-id=s:3216732167");
+
+
+            ThreadPool.QueueUserWorkItem(o => ws.OnOpen += (sender, e) => ws.Send("Hi, there!"));
+
+            ThreadPool.QueueUserWorkItem(o=>ws.OnMessage +=  (sender, e) =>
+            {
+                RunOnUiThread(() => dataSmall.Text +=  e.Data+ "\r\n");
+            });
+            ws.OnError += (sender, e) =>
+            {
+                dataBig.Text = e.Exception.ToString();
+            };
+
+            ws.OnClose += (sender, e) =>
+            {
+                dataBig.Text = "Connection Closed"; 
+            };
+            
+            submitButton.Click +=  (object senderer, EventArgs eer) =>
+            {
+
+                //////////////////////////// 
+                ThreadPool.QueueUserWorkItem(o => WSConnect(ws));   
+                ///////////////////////////
+                if (ws.IsAlive)
+                {
+                    RunOnUiThread(() => dataBig.Text = "Connection Alive");
+                }
+            };
+
+            disconnecButton.Click += (object sender, EventArgs er) =>
+            {
+                if (ws.IsAlive)
+                {
+                    ws.Close();
+                    dataBig.Text = "Connection Closed";
+                }
+                else
+                {
+                    dataBig.Text = "Not Connected";
+                }
+            };
+        }
+
+        private void WSConnect(WebSocket ws)
+        {
+            ws.Connect();
+        }
+
+    }
+}
+
---
+++ TestClient/TestClient/SubmitName.cs
@@ -0,0 +1,15 @@
+﻿using System.Text;
+using System;
+using System.Threading;
+using TestClient;
+using WebSocketSharp;
+namespace Core
+{
+    public static class SubmitName
+    {
+        public static string WebSocket(string input)
+        {
+            return (input);
+        }
+    }
+}
